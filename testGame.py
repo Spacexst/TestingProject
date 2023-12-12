6e82d474458a37ad37157012dd8ed82dc6a41e8b
AManga <anuradha012@gmail.com>
2023-12-13 00:31:07 +1300

    Add tests for gutter frame and gutter game.

--- testGame.py
+++ testGame.py
@@ -10,35 +10,42 @@ class TestBowlingGame(unittest.TestCase):
     def setUp(self):
         self.game = BowlingGame.BowlingGame()
 
+    # done
     def testRollsBetween0And10(self):
         ok = self.game.roll(3)
         assert ok == 0
-        
+       
+    # done
     def testRollsNegative(self):
         ok = self.game.roll(-5)
         assert ok == -1
         
+    # done
     def testRollsTooMany(self):
         ok = self.game.roll(12)
         assert ok == -1
 
-    
-    def testGutterGame(self):
-        ok = self.game.roll(0)
-        for i in range(0,0):
-            self.game.roll(0)
-            assert ok == 0
-            
+    # done
+    def testGutterFrame(self):
+        zeroScore1 = self.game.roll(0)
+        zeroScore2 = self.game.roll(0)
+        assert zeroScore1 == 0
+        assert zeroScore1 == zeroScore2
         
-    
-    
+    # done
+    def testGutterGame(self):
+        for i in range(10):
+            for j in range(2):
+                self.game.roll(0)
+        score = self.game.score()
+        assert score == 0
+
     def test_all_ones(self):
         ok =  self.game.roll(0)
         for i in range(1, 20):
          self.game.roll(20)  
          assert ok == 0
-         
-         
+
     def test_simple_spare(self):
         ok = self.game.roll(0) 
         for i in range(1, 20):
@@ -50,8 +57,6 @@ class TestBowlingGame(unittest.TestCase):
         for i in range(1, 20):
            self.game.roll(17) 
            assert ok == 0
-           
-    
 
     def test_perfect_game(self):
         ok = self.game.roll(0) 
@@ -59,16 +64,19 @@ class TestBowlingGame(unittest.TestCase):
         for _ in range(12):
             self.game.roll(300)
             assert ok == 0
-       
-    def test_perfect_game(self):
-        ok = self.game.roll(0) 
-        # Arrange
-        for _ in range(12):
-            self.game.roll(320)
-            assert ok ==0
-        
     
+    def testOneSpare(self):
+        ok = self.game.roll(0)
+        for _ in range(5, 21):
+            self.game.roll(150)
+            assert ok == 0
 
+    def rollMany(self, pins,rolls):
+        for i in range(rolls):
+            self.game.rolls(pins)
+                
+    
+   
     
     
            
