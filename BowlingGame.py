b67d5add69db67a214910c76ffc4bab48d180726
AManga <anuradha012@gmail.com>
2023-12-14 22:09:02 +1300

    Add text for generating documentation.

--- BowlingGame.py
+++ BowlingGame.py
@@ -2,7 +2,9 @@ class BowlingGame:
     def __init__(self):
         self.rolls=[]
 
+
     def roll(self,pins):
+        '''Save points for a roll.'''
         if (pins < 0 or pins > 10):
             return -1
         else:
@@ -10,6 +12,7 @@ class BowlingGame:
             return 0
 
     def score(self):
+        '''Calculate score for a game.'''
         result = 0
         rollIndex=0
         
@@ -29,12 +32,15 @@ class BowlingGame:
         return result
 
     def isStrike(self, rollIndex):
+        '''Returns true if a roll is a strike.'''
         return self.rolls[rollIndex] == 10
     
     def isSpare(self, rollIndex):
+        '''Returns true if the score for a frame is a spare.'''
         return self.rolls[rollIndex]+ self.rolls[rollIndex+1]==10
     
     def strikeScore(self,rollIndex):
+        '''Calculates score for a strike by inlcuding points from the next frame.'''
         score = 10 + self.rolls[rollIndex+1]
         if rollIndex < 9:
             score += self.rolls[rollIndex+2]
@@ -43,8 +49,10 @@ class BowlingGame:
         return score
 
     def spareScore(self,rollIndex):
-        return  10+ self.rolls[rollIndex+2]
+        '''Calculates score for a spare by including points from the next roll.'''
+        return  10 + self.rolls[rollIndex+2]
 
     def frameScore(self, rollIndex):
+        '''Calculates the points for a frame.'''
         return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
               
